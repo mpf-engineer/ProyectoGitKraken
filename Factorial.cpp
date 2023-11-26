97baba207455643687cc5982f375ce38726415c8
PajaroLoco <bymarkosyb@gmail.com>
2023-11-26 22:03:12 +0100

    Creación Módulo Factorial

---
+++ Factorial.cpp
@@ -0,0 +1,10 @@
+// Factorial.cpp
+#include "Factorial.h"
+
+int calcularFactorial(int numero) {
+    if (numero == 0 || numero == 1) {
+        return 1;
+    }
+    return numero * calcularFactorial(numero - 1);
+}
+
---
+++ Factorial.h
@@ -0,0 +1,8 @@
+// Factorial.h
+#ifndef FACTORIAL_H
+#define FACTORIAL_H
+
+int calcularFactorial(int numero);
+
+#endif // FACTORIAL_H
+
