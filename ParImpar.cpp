7a987cd96182dfc8eda6aba22ec5cdfa6302e7c4
Marcos <marcos.perezfernandez@alum.uca.es>
2023-11-26 22:04:13 +0100

    Creación Módulo ParImpar

---
+++ ParImpar.cpp
@@ -0,0 +1,7 @@
+// ParImpar.cpp
+#include "ParImpar.h"
+
+bool esPar(int numero) {
+    return (numero % 2 == 0);
+}
+
---
+++ ParImpar.h
@@ -0,0 +1,8 @@
+// ParImpar.h
+#ifndef PARIMPAR_H
+#define PARIMPAR_H
+
+bool esPar(int numero);
+
+#endif // PARIMPAR_H
+
